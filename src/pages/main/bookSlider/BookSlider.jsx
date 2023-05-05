f9553b30e3287fe7098277eccef034f2f6c2147c
Aristarh Gran <awebdeff@gmail.com>
2023-05-05 09:20:14 +0300

    add block bookSlider

--- src/pages/main/bookSlider/BookSlider.jsx
+++ src/pages/main/bookSlider/BookSlider.jsx
@@ -3,7 +3,24 @@ import './bookSlider.scss'
 
 const BookSlider = () => {
     return (
-        <div>BookSlider</div>
+        <div className="book-slider container">
+            <div className="book-slider__wrap">
+                <div className="book-slider__block">
+                    <span className="book-slider__block-item">
+                        Слайдер
+                    </span>
+                </div>
+            </div>
+            <div className="book-slider__book-desc">
+                <h3 className="book-link__header">Шри Намамрита:<br /> нектар святого имени</h3>
+                <div className="book-link__author">Его божественная Милость<br /> А. Ч. Бхактиведанта Свами Прабхупада</div>
+                <div className="book-slider__btn-block">
+                    <div className="book-slider__btn">Перейти в магазин</div>
+                    <div className="book-slider__price">450 р.</div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
