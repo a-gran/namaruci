d796e45f0d19267302cd62d8fc0aad8e21091b6d
Aristarh Gran <awebdeff@gmail.com>
2023-05-08 11:30:36 +0300

    add block lectures

--- src/pages/main/lectures/Lectures.jsx
+++ src/pages/main/lectures/Lectures.jsx
@@ -3,7 +3,19 @@ import './lectures.scss'
 
 const Lectures = () => {
     return (
-        <div>Lectures</div>
+        <div className="lectures container">
+            <div className="lectures__wrap">
+                <div className="lectures__block">
+                    <span className="lectures__block-item">
+                        Аудиоплеер
+                    </span>
+                </div>
+            </div>
+            <div className="set__btn-block">
+                <div className="set__btn-link">Перейти в лекторий</div>
+            </div>
+        </div>
+
     )
 }
 
