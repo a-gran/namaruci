2bbe5a398f913d3a9a8edc0f047978514cdd0506
Aristarh Gran <awebdeff@gmail.com>
2023-05-08 11:02:37 +0300

    add block video

--- src/pages/main/video/Video.jsx
+++ src/pages/main/video/Video.jsx
@@ -3,7 +3,9 @@ import "./video.scss"
 
 const Video = () => {
     return (
-        <div>Video</div>
+        <div className="container">
+            <iframe className="video" width="1280" height="720" src="https://www.youtube.com/embed/cWHTaCZyWcc" title="Что такое Веды? О чём говорят древние ведические тексты на санскрите. Сакральная философия Вед." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
     )
 }
 
