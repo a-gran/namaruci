7181485dbb147cb01e678273afc6501d41a5e636
Aristarh Gran <awebdeff@gmail.com>
2023-06-28 21:47:39 +0300

    edit first-screen

--- src/pages/main/first-screen/FirstScreen.jsx
+++ src/pages/main/first-screen/FirstScreen.jsx
@@ -10,30 +10,33 @@ const FirstScreen = () => {
 				alt="Фото"
 			/>
 
-			<div className={css.firstScreenHeader}>
-				<h1 className={`${css.heading} ${css.transition}`}>Харе Кришна!</h1>
-				<span className={`${css.greatSong} ${css.transition}`}>Великая песнь</span>
-				<span className={`${css.freedom} ${css.transition}`}>освобождения!</span>
-			</div>
-
-			<div className={css.fotoDescWrap}>
-				<div className={css.textDescWrap}>
-					<div className={css.textDescCenter}>
-						<div className={`${css.descHeader} ${css.transition}`}>
-							Духовный учитель всего мира
-						</div>
-						<div className={`${css.descHeaderText} ${css.transition}`}>
-							Его Божественная Милость Абхай Чаранаравинда Бхактиведанта
-							Свами Шрила Прабхупадa
-						</div>
-					</div>
+			<div className={css.firstScreenHeaderWrap}>
+				<div className={css.firstScreenHeader}>
+					<h1 className={`${css.heading} ${css.transition}`}>Харе Кришна!</h1>
+					<span className={`${css.greatSong} ${css.transition}`}>Великая песнь</span>
+					<span className={`${css.freedom} ${css.transition}`}>освобождения!</span>
 				</div>
 
-				<a href='#' className={css.aboutAuthor}>
-					<span className={css.aboutAuthorHeader}>
-						Об авторе
-					</span>
-				</a>
+				<div className={css.fotoDescWrap}>
+					<div className={css.textDescWrap}>
+						<div className={css.textDescCenter}>
+							<div className={`${css.descHeader} ${css.transition}`}>
+								Духовный учитель всего мира
+							</div>
+							<div className={`${css.descHeaderText} ${css.transition}`}>
+								Его Божественная Милость Абхай Чаранаравинда Бхактиведанта
+								Свами Шрила Прабхупадa
+							</div>
+						</div>
+					</div>
+
+					<a href='#' className={css.aboutAuthor}>
+						<span className={css.aboutAuthorHeader}>
+							Об авторе
+						</span>
+					</a>
+				</div>
+
 			</div>
 		</div>
 	)
